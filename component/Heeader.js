d63258c3f5037b10b07e762b5c268fcd0513b640
GaurangPatel <gp853212@gmail.com>
2021-12-26 14:40:07 +0530

    add header component

--- component/Heeader.js
+++ component/Heeader.js
@@ -1,7 +1,21 @@
+import Image from "next/image"
+import { SearchIcon } from "@heroicons/react/outline"
+
 function Heeader() {
     return (
         <div>
             <h1>Header</h1>
+            {/* HeaderLeft */}
+            <div className="flex items-center">
+                <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
+                <div className="flex ml-2 items-center rounded-full">
+                    <SearchIcon className="h-6" />
+                    <input type="text" placeholder="Search Facebook" />
+                </div>
+            </div>
+            {/* HeaderCenter */}
+
+            {/* HeaderRight */}
         </div>
     )
 }
