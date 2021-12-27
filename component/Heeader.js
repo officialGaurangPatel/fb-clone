fd3f64e48faa21f5f1f49f9ccbbf3eef93978bd5
GaurangPatel <gp853212@gmail.com>
2021-12-27 22:45:07 +0530

    changes in header

--- component/Heeader.js
+++ component/Heeader.js
@@ -1,20 +1,28 @@
 import Image from "next/image"
-import { SearchIcon } from "@heroicons/react/outline"
+import { FlagIcon, HomeIcon, PlayIcon, SearchIcon, ShoppingCartIcon, UserGroupIcon } from "@heroicons/react/outline"
+import HeaderIcon from "./HeaderIcon"
 
 function Heeader() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
             {/* HeaderLeft */}
             <div className="flex items-center">
                 <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
-                <div className="flex ml-2 items-center rounded-full">
-                    <SearchIcon className="h-6" />
-                    <input type="text" placeholder="Search Facebook" />
+                <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                    <SearchIcon className="h-6 text -gray-600" />
+                    <input type="text" placeholder="Search Facebook" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" />
                 </div>
             </div>
             {/* HeaderCenter */}
-
+            <div className="flex justify-center flex-grow">
+                <div className="flex space-x-6 md:space-x-2">
+                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
+                </div>
+            </div>
             {/* HeaderRight */}
         </div>
     )
