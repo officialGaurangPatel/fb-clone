bf34e851bcf956dcc5e1b3cb3b737cebe6887fb6
GaurangPatel <gp853212@gmail.com>
2021-12-27 23:09:46 +0530

    crate right part of header

--- component/HeaderIcon.js
+++ component/HeaderIcon.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function HeaderIcon({ Icon }) {
+function HeaderIcon({ Icon, active }) {
     return (
         <div className=' flex items-center cursor-pointer 
         md:px-10 sm:h-14 md:hover:bg-gray-100 rounded-xl active:border-b-2 active:border-blue-500 group'>
-            <Icon className='h-5 text-center group-hover:text-blue-500 sm:h-7 mx-auto' />
+            <Icon className={`h-5 text-gray-500 text-center group-hover:text-blue-500 sm:h-7 mx-auto ${active && "text-blue-500"}`} />
         </div>
     )
 }
--- component/Heeader.js
+++ component/Heeader.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { FlagIcon, HomeIcon, PlayIcon, SearchIcon, ShoppingCartIcon, UserGroupIcon } from "@heroicons/react/outline"
+import { BellIcon, ChatIcon, ChevronDownIcon, FlagIcon, HomeIcon, PlayIcon, SearchIcon, ShoppingCartIcon, UserGroupIcon, ViewGridAddIcon } from "@heroicons/react/outline"
 import HeaderIcon from "./HeaderIcon"
 
 function Heeader() {
@@ -10,13 +10,13 @@ function Heeader() {
                 <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text -gray-600" />
-                    <input type="text" placeholder="Search Facebook" className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" />
+                    <input type="text" placeholder="Search Facebook" className="hidden md:inline-flex  ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink" />
                 </div>
             </div>
             {/* HeaderCenter */}
             <div className="flex justify-center flex-grow">
                 <div className="flex space-x-6 md:space-x-2">
-                    <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={HomeIcon} active />
                     <HeaderIcon Icon={FlagIcon} />
                     <HeaderIcon Icon={PlayIcon} />
                     <HeaderIcon Icon={ShoppingCartIcon} />
@@ -24,6 +24,14 @@ function Heeader() {
                 </div>
             </div>
             {/* HeaderRight */}
+            <div className="flex items-center md:space-x-2 justify-end">
+                {/* Profile Pic */}
+                <p className="whitespace-nowrap font-semibold pr-3">Gaurang Patel</p>
+                <ViewGridAddIcon className="icon" />
+                <ChatIcon className="icon" />
+                <BellIcon className="icon" />
+                <ChevronDownIcon className="icon" />
+            </div>
         </div>
     )
 }
