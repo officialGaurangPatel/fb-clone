c27632545684569d3429b893d977cd83244d6d5d
GaurangPatel <gp853212@gmail.com>
2021-12-28 22:59:20 +0530

    add loginPage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Heeader from '../component/Heeader'
 
-export default function Home() {
+
+export default function Home({ session }) {
+  if (!session) {
+    return <Login />
+  }
   return (
     <div>
       <Head>
@@ -10,4 +15,14 @@ export default function Home() {
       <Heeader />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
