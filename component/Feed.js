0bda1623cf65e0eb0f29d81f0677a58587fffc02
GaurangPatel <gp853212@gmail.com>
2022-01-09 15:17:49 +0530

    add feed

---
+++ component/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import InputBox from './InputBox'
+import Stroies from './Stroies'
+
+function Feed() {
+    return (
+        <div className="flex flex-grow h-screen pb-44 pt-6 mt-4 xl:mr-40 overflow-y-auto">
+            <div className='mx-auto max-w-md md:max-w-lg lg:max-w-2xl'>
+                <Stroies />
+                <InputBox />
+                {/* posts */}
+            </div>
+        </div>
+    )
+}
+
+export default Feed
+
